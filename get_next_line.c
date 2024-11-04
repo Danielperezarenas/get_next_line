9ae3bf8876275093c1f25670c8251e886b1381f5
Danielperezarenas <danierr999@hotmail.com>
2024-11-04 11:06:48 +0100

    first commit to compare

---
+++ get_next_line.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danperez <danperez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 16:37:43 by danperez          #+#    #+#             */
+/*   Updated: 2024/11/02 23:21:44 by danperez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char *restante;   // Almacenamiento temporal para la próxima línea
+	size_t	BUFFER_SIZE;
+	char buffer[BUFFER_SIZE + 1];
+	int bytes_leidos;
+
+	// Leer del archivo
+	bytes_leidos = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_leidos <= 0) {
+		return (NULL);  // Fin de archivo o error
+	}
+	buffer[bytes_leidos] = '\0';
+
+	// En este punto, `buffer` contiene los datos leídos y debes agregarlos a `restante`.
+	// Puedes usar `ft_strcat` o `ft_strjoin` según tu preferencia.
+
+	return NULL;  // Para pruebas, estamos retornando NULL
+}
+}
---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danperez <danperez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 16:37:52 by danperez          #+#    #+#             */
+/*   Updated: 2024/11/02 23:21:45 by danperez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strcat(char *dest, char *src);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
+
+#endif
